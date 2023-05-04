6cc0d1ad9dea5696f96cc14fca58656b27f2b9a7
Patrick Marques <patrick.marquesmeliciano@heig-vd.ch>
2023-05-04 16:33:27 +0200

    Ajout de 2 graphs HeatMap + 1 graph Radial Stacked Bar Chart + un peu section accueil

---
+++ site-projet-sommeil/src/graph.js
@@ -0,0 +1,27 @@
+"use strict";
+
+//import * as d3 from "d3";
+
+/*
+ *
+ *
+ * HEATMAP
+ *
+ *
+ **/
+
+/*
+ *
+ *
+ * FIN HEATMAP
+ *
+ *
+ **/
+
+/*
+ *
+ *
+ * GRAPH CIRCULAIRE
+ *
+ *
+ **/
--- site-projet-sommeil/src/script.js
+++ site-projet-sommeil/src/script.js
@@ -1,13 +1,14 @@
 import * as d3 from "../node_modules/d3/dist/d3.js";
 
 // Load the CSV file using D3's built-in function
-d3.csv("data/activity.csv").then(function(data) {
-    // Convert the CSV data to a JSON object using D3's built-in function
-    var jsonData = d3.csvFormat(data);
-  
-    // Convert the JSON object to a string using JavaScript's JSON.stringify function
-    var jsonString = JSON.stringify(jsonData);
-  
-    // Log the JSON string to the console
-    console.log(jsonString);
-  });
+// d3.csv("data/activity.csv").then(function(data) {
+//     // Convert the CSV data to a JSON object using D3's built-in function
+//     var jsonData = d3.csvFormat(data);
+
+//     // Convert the JSON object to a string using JavaScript's JSON.stringify function
+//     var jsonString = JSON.stringify(jsonData);
+
+//     // Log the JSON string to the console
+//     console.log(jsonString);
+//   });
+//
