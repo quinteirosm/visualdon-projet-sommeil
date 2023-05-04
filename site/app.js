893c7be58813829d2ac7d1ee606503637a4d335f
Patrick Marques <patrick.marquesmeliciano@heig-vd.ch>
2023-05-04 13:51:22 +0200

    Suppression du dossier site

--- site/app.js
+++
@@ -1,6 +0,0 @@
-import * as d3 from "https://cdnjs.cloudflare.com/ajax/libs/d3/7.8.4/d3.js";
-import { csv } from "d3";
-
-d3.csv("/data/activity.csv").then(function(data) {
-    console.log(data);
-    });
