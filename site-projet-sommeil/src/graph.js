c952e1dad65622c3fa7914a824eae01881e25175
Patrick Marques <patrick.marquesmeliciano@heig-vd.ch>
2023-05-08 22:36:34 +0200

    Réussite heatmap apnée Patrick

--- site-projet-sommeil/src/graph.js
+++ site-projet-sommeil/src/graph.js
@@ -22,141 +22,222 @@ const margin = {
  *
  **/
 
-const heatmap = (name, donnees, commentaire) => {
-	// append the svg object to the body of the page
-	const svg = d3
-		.select(name)
-		.append("svg")
-		.attr("width", width + margin.left + margin.right)
-		.attr("height", height + margin.top + margin.bottom)
-		.append("g")
-		.attr("transform", `translate(${margin.left}, ${margin.top})`);
+// const heatmap = (name, donnees, commentaire) => {
+// 	// append the svg object to the body of the page
+// 	const svg = d3
+// 		.select(name)
+// 		.append("svg")
+// 		.attr("width", width + margin.left + margin.right)
+// 		.attr("height", height + margin.top + margin.bottom)
+// 		.append("g")
+// 		.attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-	//Read the data
-	d3
-		.csv(
-			"https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv"
-		)
+// 	//Read the data
+// 	d3
+// 		.csv(
+// 			"https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/heatmap_data.csv"
+// 		)
 
-		.then(function (data) {
-			// Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
-			const myGroups = Array.from(new Set(data.map((d) => d.group)));
-			const myVars = Array.from(new Set(data.map((d) => d.variable)));
+// 		.then(function (data) {
+// 			// Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
+// 			const myGroups = Array.from(new Set(data.map((d) => d.group)));
+// 			const myVars = Array.from(new Set(data.map((d) => d.variable)));
 
-			// Build X scales and axis:
-			const x = d3.scaleBand().range([0, width]).domain(myGroups).padding(0.05);
-			svg
-				.append("g")
-				.style("font-size", 15)
-				.attr("transform", `translate(0, ${height})`)
-				.call(d3.axisBottom(x).tickSize(0))
-				.select(".domain")
-				.remove();
+// 			// Build X scales and axis:
+// 			const x = d3.scaleBand().range([0, width]).domain(myGroups).padding(0.05);
+// 			svg
+// 				.append("g")
+// 				.style("font-size", 15)
+// 				.attr("transform", `translate(0, ${height})`)
+// 				.call(d3.axisBottom(x).tickSize(0))
+// 				.select(".domain")
+// 				.remove();
 
-			// Build Y scales and axis:
-			const y = d3.scaleBand().range([height, 0]).domain(myVars).padding(0.05);
-			svg
-				.append("g")
-				.style("font-size", 15)
-				.call(d3.axisLeft(y).tickSize(0))
-				.select(".domain")
-				.remove();
+// 			// Build Y scales and axis:
+// 			const y = d3.scaleBand().range([height, 0]).domain(myVars).padding(0.05);
+// 			svg
+// 				.append("g")
+// 				.style("font-size", 15)
+// 				.call(d3.axisLeft(y).tickSize(0))
+// 				.select(".domain")
+// 				.remove();
 
-			// Build color scale
-			const myColor = d3
-				.scaleSequential()
-				.interpolator(d3.interpolateInferno)
-				.domain([1, 100]);
+// 			// Build color scale
+// 			const myColor = d3
+// 				.scaleSequential()
+// 				.interpolator(d3.interpolateInferno)
+// 				.domain([1, 100]);
 
-			// create a tooltip
-			const tooltip = d3
-				.select(name)
-				.append("div")
-				.style("opacity", 0)
-				.attr("class", "tooltip")
-				.style("background-color", "white")
-				.style("border", "solid")
-				.style("border-width", "2px")
-				.style("border-radius", "5px")
-				.style("padding", "5px");
+// 			// create a tooltip
+// 			const tooltip = d3
+// 				.select(name)
+// 				.append("div")
+// 				.style("opacity", 0)
+// 				.attr("class", "tooltip")
+// 				.style("background-color", "white")
+// 				.style("border", "solid")
+// 				.style("border-width", "2px")
+// 				.style("border-radius", "5px")
+// 				.style("padding", "5px");
 
-			// Three function that change the tooltip when user hover / move / leave a cell
-			const mouseover = function (event, d) {
-				tooltip.style("opacity", 1);
-				d3.select(this).style("stroke", "black").style("opacity", 1);
-			};
-			const mousemove = function (event, d) {
-				tooltip
-					.html("The exact value of<br>this cell is: " + d.value)
-					.style("left", event.x / 2 + "px")
-					.style("top", event.y / 2 + "px");
-			};
-			const mouseleave = function (event, d) {
-				tooltip.style("opacity", 0);
-				d3.select(this).style("stroke", "none").style("opacity", 0.8);
-			};
+// 			// Three function that change the tooltip when user hover / move / leave a cell
+// 			const mouseover = function (event, d) {
+// 				tooltip.style("opacity", 1);
+// 				d3.select(this).style("stroke", "black").style("opacity", 1);
+// 			};
+// 			const mousemove = function (event, d) {
+// 				tooltip
+// 					.html("The exact value of<br>this cell is: " + d.value)
+// 					.style("left", event.x / 2 + "px")
+// 					.style("top", event.y / 2 + "px");
+// 			};
+// 			const mouseleave = function (event, d) {
+// 				tooltip.style("opacity", 0);
+// 				d3.select(this).style("stroke", "none").style("opacity", 0.8);
+// 			};
 
-			// add the squares
-			svg
-				.selectAll()
-				.data(data, function (d) {
-					return d.group + ":" + d.variable;
-				})
-				.join("rect")
-				.attr("x", function (d) {
-					return x(d.group);
-				})
-				.attr("y", function (d) {
-					return y(d.variable);
-				})
-				.attr("rx", 4)
-				.attr("ry", 4)
-				.attr("width", x.bandwidth())
-				.attr("height", y.bandwidth())
-				.style("fill", function (d) {
-					return myColor(d.value);
-				})
-				.style("stroke-width", 4)
-				.style("stroke", "none")
-				.style("opacity", 0.8)
-				.on("mouseover", mouseover)
-				.on("mousemove", mousemove)
-				.on("mouseleave", mouseleave);
+// 			// add the squares
+// 			svg
+// 				.selectAll()
+// 				.data(data, function (d) {
+// 					return d.group + ":" + d.variable;
+// 				})
+// 				.join("rect")
+// 				.attr("x", function (d) {
+// 					return x(d.group);
+// 				})
+// 				.attr("y", function (d) {
+// 					return y(d.variable);
+// 				})
+// 				.attr("rx", 4)
+// 				.attr("ry", 4)
+// 				.attr("width", x.bandwidth())
+// 				.attr("height", y.bandwidth())
+// 				.style("fill", function (d) {
+// 					return myColor(d.value);
+// 				})
+// 				.style("stroke-width", 4)
+// 				.style("stroke", "none")
+// 				.style("opacity", 0.8)
+// 				.on("mouseover", mouseover)
+// 				.on("mousemove", mousemove)
+// 				.on("mouseleave", mouseleave);
+// 		});
+
+// 	// Add title to graph
+// 	svg
+// 		.append("text")
+// 		.attr("x", 0)
+// 		.attr("y", -50)
+// 		.attr("text-anchor", "left")
+// 		.style("font-size", "22px")
+// 		.text("A d3.js heatmap");
+
+// 	// Add subtitle to graph
+// 	svg
+// 		.append("text")
+// 		.attr("x", 0)
+// 		.attr("y", -20)
+// 		.attr("text-anchor", "left")
+// 		.style("font-size", "14px")
+// 		.style("fill", "grey")
+// 		.style("max-width", 400)
+// 		.text(commentaire);
+// };
+
+// dataCpap.then((data) => {
+// 	console.log("essai HEATMAP CPAP PATRICK");
+// 	data.forEach((element) => {
+// 		console.log(element.evenementHeure + " " + element.date);
+// 	});
+// });
+
+// heatmap(".heatmap_profond_patrick");
+
+const heatmapApneePatrick = (name, donnees) => {
+	const width = 600;
+	const height = 400;
+	const dates = [];
+	const values = [];
+
+	dataCpap.then((dataRecue) => {
+		console.log("essai HEATMAP CPAP PATRICK");
+
+		let maxValue = 0;
+
+		dataRecue.forEach((element) => {
+			console.log(element.evenementHeure + " " + element.date);
+			dates.push(element.date);
+			values.push(element.evenementHeure);
+
+			if (element.evenementHeure > maxValue) {
+				maxValue = element.evenementHeure;
+			}
 		});
 
-	// Add title to graph
-	svg
-		.append("text")
-		.attr("x", 0)
-		.attr("y", -50)
-		.attr("text-anchor", "left")
-		.style("font-size", "22px")
-		.text("A d3.js heatmap");
+		const xScale = d3
+			.scaleTime()
+			.domain([new Date(dates[0]), new Date(dates[dates.length - 1])])
+			.range([0, width]);
 
-	// Add subtitle to graph
-	svg
-		.append("text")
-		.attr("x", 0)
-		.attr("y", -20)
-		.attr("text-anchor", "left")
-		.style("font-size", "14px")
-		.style("fill", "grey")
-		.style("max-width", 400)
-		.text(commentaire);
+		const colorScale = d3
+			.scaleLinear()
+			.domain([0, maxValue])
+			.range(["#00FFFF", "#000000"]);
+
+		let compteur = 0;
+		const nbCols = 4; // Nombre de colonnes
+		const data = [];
+		for (let i = 0; i < dates.length; i++) {
+			data[i] = [];
+			for (let j = 0; j < nbCols; j++) {
+				data[i][j] = values[compteur];
+				compteur++;
+				// data[i][j] = Math.floor(Math.random() * 11); // Remplacer par vos propres données
+			}
+		}
+
+		// const rectWidth = width / (dates.length * nbCols);
+		// const rectHeight = height / nbCols;
+
+		const rectWidth = 50;
+		const rectHeight = 50;
+
+		const svg = d3
+			.select(name)
+			.append("svg")
+			.attr("width", width)
+			.attr("height", height);
+
+		const rows = svg
+			.selectAll("g")
+			.data(data)
+			.enter()
+			.append("g")
+			.attr("transform", (d, i) => `translate(0, ${i * rectHeight})`);
+
+		const rects = rows
+			.selectAll("rect")
+			.data((d) => d)
+			.enter()
+			.append("rect")
+			.attr(
+				"x",
+				(d, i) =>
+					(i % nbCols) * rectWidth +
+					Math.floor(i / nbCols) * dates.length * rectWidth
+			)
+			.attr("y", (d, i) => Math.floor(i / nbCols) * rectHeight)
+			.attr("width", rectWidth)
+			.attr("height", rectHeight)
+			.attr("fill", (d) => colorScale(d));
+	});
 };
 
-dataCpap.then((data) => {
-	console.log("essai HEATMAP CPAP PATRICK");
-	data.forEach((element) => {
-		console.log(element.evenementHeure + " " + element.date);
-	});
-});
+heatmapApneePatrick(".heatmap_nb_apnee", dataCpap);
 
-heatmap(".heatmap_profond_patrick");
-heatmap(".heatmap_nb_apnee", dataCpap, "commentaire");
-
-heatmap(".heatmap_profond_patrick_deux");
-heatmap(".heatmap_profond_miguel");
+heatmapApneePatrick(".heatmap_profond_patrick_deux", dataCpap);
+// heatmap(".heatmap_profond_miguel");
 
 /*
  *
