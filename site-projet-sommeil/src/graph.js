d392ae889bd6bc0ac10c01d980708ec1af579dc5
Patrick Marques <patrick.marquesmeliciano@heig-vd.ch>
2023-05-08 09:46:06 +0200

    Tentative fusion

--- site-projet-sommeil/src/graph.js
+++ site-projet-sommeil/src/graph.js
@@ -167,7 +167,6 @@ const bar = (name, donnees) => {
 					// console.log(jour);
 					// console.log(mois);
 					// return `${jour}.${mois}`;
-					d.date.setHours(0, 0, 0, 0);
 					return d.date;
 				})
 			)
